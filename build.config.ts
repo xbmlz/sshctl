1727ed0521296efd76dece5adb0beed882636ede
xbmlz <997909544@qq.com>
2023-03-10 15:48:33 +0800

    first commit

---
+++ build.config.ts
@@ -0,0 +1,11 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: ['src/index.ts'],
+  clean: true,
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+    inlineDependencies: true,
+  },
+})
